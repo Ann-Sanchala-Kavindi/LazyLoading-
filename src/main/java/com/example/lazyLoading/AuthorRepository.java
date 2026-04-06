fa3632b2ac545daeff6f4e48991feccc954c90ec
Ann-Sanchala-Kavindi <annsanchala@gmail.com>
2026-04-06 16:55:26 +0530

    Controller modification

--- src/main/java/com/example/lazyLoading/AuthorRepository.java
+++ src/main/java/com/example/lazyLoading/AuthorRepository.java
@@ -2,9 +2,25 @@ package com.example.lazyLoading;
 
 
 import com.example.lazyLoading.model.Author;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author,Long> {
+
+    // For lazy fix — JOIN FETCH courses
+    @Query("SELECT a FROM Author a JOIN FETCH a.books WHERE a.id = :id")
+    Optional<Author> findByIdWithBooks(@Param("id") Long id);
+
+
+    @EntityGraph(attributePaths = {"books"})
+    @Query("FROM Author ")
+    List<Author> findAllAuthors();
+
 
 
 }
--- src/main/java/com/example/lazyLoading/controller/AuthorController.java
+++ src/main/java/com/example/lazyLoading/controller/AuthorController.java
@@ -17,11 +17,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthorController {
 
-    @Autowired
+
     private final AuthorRepository authorRepository;
 
-    //Method01 - LazyIntializationException
-    //Session closes after findById(), Try to accesss books outside the session
+    //-----Method01 - LazyIntializationException----------------------------------------------------------
+    //Session closes after findById(), Try to access books outside the session
     @GetMapping("/authors/{id}/no-lazy")
     public void getLazyError(@PathVariable Long id){
         System.out.println("get authors ");
@@ -35,7 +35,7 @@ public class AuthorController {
 
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     @GetMapping("/authors/{id}/lazy")
     public void getAuthor(@PathVariable Long id){
 
@@ -48,6 +48,58 @@ public class AuthorController {
         System.out.println(books.size());
 
     }
+
+
+
+
+    //---Method 02 -Correct Lazy Loading--------------------------------------------------------
+    //Uses JOIN FETCH query — loads courses WITHIN the session
+    @GetMapping("/authors/{id}/books")
+    public void getAuthorWithBooks(@PathVariable Long id){
+        System.out.println("get author");
+
+        //Fetches author+books in ONE Query(session still open)
+        Author author=authorRepository.findByIdWithBooks(id)
+                .orElseThrow(() -> new RuntimeException("Author Not Found"));
+
+        System.out.println("Getting Books");
+        // books already loaded - no extra query, no exception
+        var books=author.getBooks();
+
+        System.out.println(books.size());
+
+    }
+
+
+    // METHOD 3 — All Authors, each with their books(N+1 issue)--------------------------------
+    //@Transactional(readOnly = true) - not needed
+    @GetMapping("/authors/books/n+1issue")
+    public void getAllAuthorsWithBooksIssue(){
+
+        System.out.println("Getting the Authors");
+        List<Author> authors=authorRepository.findAll();
+
+        System.out.println("Getting each author books");
+        for(Author author:authors){
+            System.out.println(author.getBooks().size());
+        }
+    }
+
+    // METHOD 4 — All Authors, each with their books(N+1 issue fixed)
+    //@Transactional(readOnly = true) - not needed
+    @GetMapping("/authors/books/n+1fixed")
+    public void getAllAuthorsWithBooksFixed() {
+
+        System.out.println("getting the authors");
+        List<Author> authors = authorRepository.findAllAuthors(); // only student rows
+
+        System.out.println("getting each authors books");
+        for (Author author:authors) {
+            System.out.println(author.getBooks().size());
+        }
+    }
+
+
 
 
 
